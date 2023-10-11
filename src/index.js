28a771cc0534b11ab0c92eb104f308ce74b8e587
Fernanda Schonffeldt <f.schonffeldtc@gmail.com>
2023-10-10 22:01:24 -0300

    conexion a la base de datos exitosa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express, { json } from 'express'
+import { Prueba } from './config/env.config.js';
+import { setupDB } from './config/db.config.js';
 const app = express()
 const port = 3000
 
@@ -11,4 +13,5 @@ app.get('/api', (_req, res) => {
 app.listen(port, () => {
   console.log(`API en la url http://localhost:${port}`)
   console.log(`la API funciona: ${process.env.Prueba}`);
+  setupDB()
 })
