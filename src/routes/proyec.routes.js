c1c36b2bdbac9f36455d58a7416e650d4c81da33
dampuero <ampuero.diegob@gmail.com>
2023-10-30 00:20:13 -0300

    Controller update

--- src/routes/proyec.routes.js
+++ src/routes/proyec.routes.js
@@ -1,13 +1,18 @@
-const Router = express.Router();
+"use strict";
+const express = require("express");
 
-const proyectController = require('../controllers/proyect.controller.js');
-const createProyec = proyectController.createProyec;
-const getProyecs = proyectController.getProyecs;
+const proyecController = require("../controllers/proyect.controller.js");
+const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
+const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
 
+const router = express.Router();
 
-const router = Router();
+router.use(authenticationMiddleware);
 
-router.get('/', getProyecs);
-router.post('/', createProyec);
+router.get("/", proyecController.getProyectos);
+router.post("/", authorizationMiddleware.isAdmin, proyecController.createProyecto);
+router.get("/:id", proyecController.getProyectoById);
+router.put("/:id", authorizationMiddleware.isAdmin, proyecController.updateProyecto);
+router.delete("/:id", authorizationMiddleware.isAdmin, proyecController.deleteProyecto);
 
-export default router;
+module.exports = router;
