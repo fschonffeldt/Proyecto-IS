b362b2e77ac9c6605dda36e633f024ef225de06d
Fernanda Schonffeldt <f.schonffeldtc@gmail.com>
2023-10-30 00:49:11 -0300

    intento validacion concurso/Admin

--- src/routes/concurso.routes.js
+++ src/routes/concurso.routes.js
@@ -3,12 +3,21 @@
 
 const express = require("express");
 const concursoController = require("../controllers/concurso.controller");
+const authenticationMiddleware = require('../middlewares/authentication.middleware');
+const { isAdmin } = require('../middlewares/authorization.middleware');
 
 const router = express.Router();
 
-router.get('/', concursoController.findAll);  // Ruta para obtener todos los concursos
-router.post('/', concursoController.create);  // Ruta para crear un nuevo concurso
-router.put('/:id', concursoController.update);  // Ruta para actualizar un concurso existente
-router.delete('/:id', concursoController.delete);  // Ruta para eliminar un concurso existente
+// Ruta para obtener todos los concursos. Todos los usuarios pueden acceder.
+router.get('/', authenticationMiddleware, concursoController.findAll);
+
+// Ruta para crear un nuevo concurso. Solo accesible para administradores.
+router.post('/', authenticationMiddleware, isAdmin, concursoController.create);
+
+// Ruta para actualizar un concurso existente. Solo accesible para administradores.
+router.put('/:id', authenticationMiddleware, isAdmin, concursoController.update);
+
+// Ruta para eliminar un concurso existente. Solo accesible para administradores.
+router.delete('/:id', authenticationMiddleware, isAdmin, concursoController.delete);
 
 module.exports = router;
