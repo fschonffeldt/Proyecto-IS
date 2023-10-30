fb5c4ada8a4615326736c4e6aac6f81ca4dd17d6
dampuero <ampuero.diegob@gmail.com>
2023-10-30 09:39:31 -0300

    CRUD exitoso

--- src/models/proyec.model.js
+++ src/models/proyec.model.js
@@ -1,4 +1,6 @@
-import { Schema, model } from 'mongoose';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const model = mongoose.model;
 
 const proyecSchema = new Schema(
     {
@@ -10,4 +12,4 @@ const proyecSchema = new Schema(
     }, 
     { collection: 'proyectos',  versionKey: false, timestamps: true,});
 
-export default model('Proyec', proyecSchema);
+module.exports = model('Proyec', proyecSchema);
