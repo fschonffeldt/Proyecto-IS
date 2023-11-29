da24afbfa1a9f94096af474355c3804468548a80
GiulianoNeira <luis.acuna2101@alumnos.ubiobio.cl>
2023-11-29 15:28:19 -0300

    creacion de usuario y roles en evaluacion

---
+++ src/constants/estado.constants.js
@@ -0,0 +1,8 @@
+/** Roles permitidos por la base de datos */
+const ESTADOS = {
+    ACEPTADO: "Aceptado",
+    EN_REVISION: "En revisión",
+    RECHAZADO: "Rechazado"
+  };
+  
+  module.exports = ESTADOS;
--- src/routes/evaluacion.routes.js
+++ src/routes/evaluacion.routes.js
@@ -4,13 +4,22 @@
 const express = require("express");
 const evaluacionController = require("../controllers/evaluacion.controller");
 
+/** Middlewares de autorización */
+const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
+
+/** Middleware de autenticación */
+const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
+
+// Define el middleware de autenticación para todas las rutas
+router.use(authenticationMiddleware);
+
 const router = express.Router();
 
-router.get('/', evaluacionController.getEvaluacion);  // Ruta para obtener todos los fondos
-router.post('/', evaluacionController.createEvaluacion);  // Ruta para crear un nuevo fondo
-router.put('/:id', evaluacionController.updateEvaluacion);  // Ruta para actualizar un fondo existente
-router.delete('/:id', evaluacionController.deleteEvaluacion);  // Ruta para eliminar un fondo existente
-router.get('/:id', evaluacionController.getEvaluacionById);
-router.get('/:id_postulacion', evaluacionController.getEvaluacionByPostulacion);
+router.get('/', authorizationMiddleware.isEvaluador, evaluacionController.getEvaluacion);  // Ruta para obtener todos los fondos
+router.post('/', authorizationMiddleware.isEvaluador, evaluacionController.createEvaluacion);  // Ruta para crear un nuevo fondo
+router.put('/:id', authorizationMiddleware.isEvaluador, evaluacionController.updateEvaluacion);  // Ruta para actualizar un fondo existente
+router.delete('/:id', authorizationMiddleware.isEvaluador, evaluacionController.deleteEvaluacion);  // Ruta para eliminar un fondo existente
+router.get('/:id', authorizationMiddleware.isEvaluador, authorizationMiddleware.isUser, evaluacionController.getEvaluacionById);
+router.get('/:id_postulacion', authorizationMiddleware.isUser, evaluacionController.getEvaluacionByPostulacion);
 
 module.exports = router;
