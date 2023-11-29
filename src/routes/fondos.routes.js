d1b5d8836c1c20f7dd9e43f7abcbd33ff95c2f90
Fernanda Schonffeldt <f.schonffeldtc@gmail.com>
2023-11-29 17:43:22 -0300

    solucion fondos routes

--- src/routes/fondos.routes.js
+++ src/routes/fondos.routes.js
@@ -2,15 +2,15 @@
 "use strict";
 
 const express = require("express");
-const fondoController = require("../controllers/fondos.controller");
+const fondosController = require("../controllers/fondos.controller");
 const authenticationMiddleware = require('../middlewares/authentication.middleware');
 
 const { isAdmin, isUser } = require('../middlewares/authorization.middleware');
 const router = express.Router();
 
-router.get('/',authenticationMiddleware,isUser, fondoController.findAll);  // Ruta para obtener todos los fondos
-router.post('/',authenticationMiddleware, isAdmin, fondoController.create);  // Ruta para crear un nuevo fondo
-router.put('/:id',authenticationMiddleware, isAdmin, fondoController.update);  // Ruta para actualizar un fondo existente
-router.delete('/:id',authenticationMiddleware, isAdmin, fondoController.delete);  // Ruta para eliminar un fondo existente
+router.get('/',authenticationMiddleware,isUser, fondosController.findAll);  // Ruta para obtener todos los fondos
+router.post('/',authenticationMiddleware, isAdmin, fondosController.create);  // Ruta para crear un nuevo fondo
+router.put('/:id',authenticationMiddleware, isAdmin, fondosController.update);  // Ruta para actualizar un fondo existente
+router.delete('/:id',authenticationMiddleware, isAdmin, fondosController.delete);  // Ruta para eliminar un fondo existente
 
 module.exports = router;
