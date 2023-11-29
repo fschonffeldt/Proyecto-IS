96b156c2160f680d5ecc6742c86d9d48500c5063
Fernanda Schonffeldt <f.schonffeldtc@gmail.com>
2023-11-29 17:42:18 -0300

    main funcional

--- src/routes/fondos.routes.js
+++ src/routes/fondos.routes.js
@@ -1,16 +1,24 @@
-// routes/fondo.routes.js
+// fondos.routes.js
+
 "use strict";
 
 const express = require("express");
-const fondoController = require("../controllers/fondos.controller");
+const fondosController = require("../controllers/fondos.controller");
 const authenticationMiddleware = require('../middlewares/authentication.middleware');
+const { isAdmin } = require('../middlewares/authorization.middleware');
 
-const { isAdmin, isUser } = require('../middlewares/authorization.middleware');
 const router = express.Router();
 
-router.get('/',authenticationMiddleware,isUser, fondoController.findAll);  // Ruta para obtener todos los fondos
-router.post('/',authenticationMiddleware, isAdmin, fondoController.create);  // Ruta para crear un nuevo fondo
-router.put('/:id',authenticationMiddleware, isAdmin, fondoController.update);  // Ruta para actualizar un fondo existente
-router.delete('/:id',authenticationMiddleware, isAdmin, fondoController.delete);  // Ruta para eliminar un fondo existente
+// Ruta para obtener todos los fondos. Todos los usuarios pueden acceder.
+router.get('/', authenticationMiddleware, fondosController.findAll);
+
+// Ruta para crear un nuevo fondo. Solo accesible para administradore
+router.post('/', authenticationMiddleware, isAdmin, fondosController.create);
+
+// Ruta para actualizar un fondo existente. Solo accesible para administradores.
+router.put('/:id', authenticationMiddleware, isAdmin, fondosController.update);
+
+// Ruta para eliminar un fondo existente. Solo accesible para administradores.
+router.delete('/:id', authenticationMiddleware, isAdmin, fondosController.delete);
 
 module.exports = router;
