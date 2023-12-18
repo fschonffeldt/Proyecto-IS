838e78513ed24f226f800f64508028c3337c28b2
dampuero <ampuero.diegob@gmail.com>
2023-12-18 20:20:35 -0300

    Add CrearProyec w/ login

--- frontend/src/components/CrearProyec.jsx
+++ frontend/src/components/CrearProyec.jsx
@@ -1,58 +1,134 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useForm } from "react-hook-form";
+import axios from '../services/root.service';
 
 const ProyectoForm = () => {
-  const [proyecto, setProyecto] = useState({
-    Tema: '',
-    Descripcion: '',
-    Monto: 0,
-    Bases: ''
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProyecto((prevProyecto) => ({
-      ...prevProyecto,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      await axios.post('http://localhost:5000/api/proyec/crear', proyecto);
-      console.log('Proyecto creado exitosamente');
-      // Puedes redirigir a otra página o realizar otras acciones después de la creación
-    } catch (error) {
-      console.error('Error al crear el proyecto', error);
-    }
+  const onSubmit = (data) => {
+      console.log(data);
+      axios.post('/proyec/crear', data)
+          .then((response) => {
+              console.log(response.data);
+              reset(); 
+          })
+          .catch((error) => {
+              console.error('Error al enviar los datos:', error);
+          });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Tema:
-        <input type="text" name="Tema" value={proyecto.Tema} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        Descripcion:
-        <textarea name="Descripcion" value={proyecto.Descripcion} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        Monto:
-        <input type="number" name="Monto" value={proyecto.Monto} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        Bases:
-        <input type="text" name="Bases" value={proyecto.Bases} onChange={handleChange} />
-      </label>
-      <br />
-      <button type="submit">Crear Proyecto</button>
-    </form>
+    <div>
+        <div className="mt-10">
+            <form className="mx-auto max-w-sm" onSubmit={handleSubmit(onSubmit)}>
+                
+                <div className="col-span-full">
+                    <label htmlFor="Tema" className="block text-sm font-medium leading-6 text-gray-900">
+                        Tema
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            {...register("Tema", {
+                                pattern: {
+                                    value: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+                                    message: "El tema debe tener entre 1 y 40 letras."
+                                }
+                            })}
+                            id="Tema"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.Tema && (
+                            <span className="text-red-500">{errors.Tema.message}</span>
+                        )}
+                    </div>
+                </div>
+
+                <div className="col-span-full">
+                    <label htmlFor="Descripcion" className="block text-sm font-medium leading-6 text-gray-900">
+                        Descripcion
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            {...register("Descripcion", {
+                                pattern: {
+                                    value: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+                                    message: "El tema debe tener entre 1 y 40 letras."
+                                }
+                            })}
+                            id="Descripcion"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.Tema && (
+                            <span className="text-red-500">{errors.Descripcion.message}</span>
+                        )}
+                    </div>
+                </div>
+
+                <div className="col-span-full">
+                  <label htmlFor="Monto" className="block text-sm font-medium leading-6 text-gray-900">
+                    Monto
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="number"
+                      {...register("Monto", {
+                        min: {
+                          value: 0,
+                          message: "El monto debe ser igual o mayor que 0."
+                        },
+                        max: {
+                          value: 999999999,
+                          message: "El monto no puede exceder 999999999"
+                        }
+                      })}
+                      id="Monto"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                    {errors.Monto && (
+                      <span className="text-red-500">{errors.Monto.message}</span>
+                    )}
+                  </div>
+                </div>
+
+                <div className="col-span-full">
+                    <label htmlFor="Bases" className="block text-sm font-medium leading-6 text-gray-900">
+                        Bases
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            {...register("Bases", {
+                                pattern: {
+                                    value: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+                                    message: "BASES debe tener entre 1 y 40 letras."
+                                }
+                            })}
+                            id="Bases"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        {errors.Tema && (
+                            <span className="text-red-500">{errors.Bases.message}</span>
+                        )}
+                    </div>
+                </div>
+
+                <div className="mt-6 flex items-center justify-center gap-x-6">
+                  <button
+                    type="submit"
+                    className="rounded-md bg-azul px-8 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Enviar
+                  </button>
+                </div>
+            </form>
+        </div>
+    </div>
   );
 };
 
