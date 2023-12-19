9f828221dd4631a556257d46e8201c39962e2b10
Ninxsk <ninoskaparedes@gmail.com>
2023-12-18 23:32:29 -0300

    appcontent

--- frontend/src/components/Appcontent.jsx
+++ frontend/src/components/Appcontent.jsx
@@ -1,24 +1,23 @@
+
 const AppContent = () => {
+  return (
+    <main className="container mt-3">
+      <div className="row">
+        <div className="col-md-12 text-center mb-3">
+          {/* Imagen en el centro */}
+          <img src="./assets/camion.jpg" alt="Municipio"/>
+        </div>
+        <div className="col-md-8">
+          {/* Contenido de la municipalidad */}
+          <h2 className="mb-4">Información Municipal</h2>
+          <p>
+            Bienvenido al Municipio. Aquí puedes descubrir todo lo relacionado con nuestra comunidad.
+            Te ofrecemos servicios, trámites y noticias importantes para ti.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-        return (
-          <main className="container mt-3">
-            <div className="row">
-              <div className="col-md-12 text-center mb-3">
-                {/* Imagen en el centro */}
-                <img src="/assets/camion.jpg" alt="Municipalidad" className="img-fluid" />
-              </div>
-              <div className="col-md-8">
-                {/* Contenido de la municipalidad */}
-                <h2>Información Municipal</h2>
-                <p>Descubre todo lo relacionado con nuestra municipalidad.</p>
-              </div>
-              <div className="col-md-4">
-                {/* Contenido de las opciones */}
-
-              </div>
-            </div>
-          </main>
-        );
-      }
-
-  export default AppContent;
+export default AppContent;
