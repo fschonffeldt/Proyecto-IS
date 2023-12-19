e667dc1980de9719b69405b13e5f7da08dc458ad
Fernanda Schonffeldt <f.schonffeldtc@gmail.com>
2023-12-18 23:15:29 -0300

    a3

--- frontend/src/components/FondoForm.jsx
+++ frontend/src/components/FondoForm.jsx
@@ -1,45 +1,46 @@
-
 import { useForm } from 'react-hook-form';
-import { createfondos } from '../services/fondos.service'; // Ajusta la ruta según tu estructura
+import { createfondos } from '../services/fondos.service';
 import { useNavigate } from 'react-router-dom';
+import './FondoForm.css';
+
 const FondoForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const navigate = useNavigate();
-  
-    const onSubmit = async (data) => {
-      try {
-        const response = await createfondos(data);
-        if (response) {
-          console.log(response); // Maneja la respuesta según tus necesidades
-          navigate('/'); // Redirecciona a la lista de fondos después de crear
-        }
-      } catch (error) {
-        console.error('Error al enviar el formulario de fondo:', error);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await createfondos(data);
+      if (response) {
+        console.log(response);
+        navigate('/');
       }
-    };
-  
-    return (
-      <div className="form-container">
-        <h2>Nuevo Fondo</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <label htmlFor="nombre">Nombre del fondo:</label>
-            <input {...register('nombre', { required: 'Este campo es obligatorio' })} />
-            {errors.nombre && <span className="error-message">{errors.nombre.message}</span>}
-          </div>
-  
-          <div className="form-group">
-            <label htmlFor="montoTotal">Monto Total:</label>
-            <input {...register('montoTotal', { required: 'Este campo es obligatorio', min: { value: 1, message: 'El monto debe ser mayor que cero' } })} />
-            {errors.montoTotal && <span className="error-message">{errors.montoTotal.message}</span>}
-          </div>
-  
-          {/* Agrega más campos según tus necesidades */}
-  
-          <button type="submit">Crear Fondo</button>
-        </form>
-      </div>
-    );
+    } catch (error) {
+      console.error('Error al enviar el formulario de fondo:', error);
+    }
   };
-  
-  export default FondoForm;
+
+  return (
+    <div className="container mt-5">
+      <h2>Nuevo Fondo</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="nombre" className="form-label">Nombre del fondo:</label>
+          <input {...register('nombre', { required: 'Este campo es obligatorio' })} className={form-control ${errors.nombre ? 'is-invalid' : ''}} />
+          {errors.nombre && <div className="invalid-feedback">{errors.nombre.message}</div>}
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="montoTotal" className="form-label">Monto Total:</label>
+          <input {...register('montoTotal', { required: 'Este campo es obligatorio', min: { value: 1, message: 'El monto debe ser mayor que cero' } })} className={form-control ${errors.montoTotal ? 'is-invalid' : ''}} />
+          {errors.montoTotal && <div className="invalid-feedback">{errors.montoTotal.message}</div>}
+        </div>
+
+        {/* Agrega más campos según tus necesidades */}
+
+        <button type="submit" className="btn btn-primary">Crear Fondo</button>
+      </form>
+    </div>
+  );
+};
+
+export default FondoForm;
